2af5f509823cc8ae87376d6e915acf53e1e1171b
Vladimir <54140425+VladimirKhara@users.noreply.github.com>
2025-01-03 18:45:19 +0800

    test

--- SpyChat/RestAPI/views.py
+++ SpyChat/RestAPI/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import PictureForm
+
+def uploadForm(req):
+    if req.method == 'POST':
+        form = PictureForm(req.POST, req.FILES)
+        if form.is_valid(): 
+            form.save()
+            return redirect('/admin')
+            #  handle_uploaded_file(request.FILES["file"])
+        else:
+            context = {'form': form}
+            return render(req, 'upload.html', context)
+    context = {'form': PictureForm()}
+    return render(req, 'upload.html', context)
+
--- SpyChat/SpyChat/settings.py
+++ SpyChat/SpyChat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(4f5i-gx^6p$5ceh1-q3he88a$x5!7+v0s41(6o)$^8vpzm6++
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['45.153.68.127']
+ALLOWED_HOSTS = ['45.153.68.127', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'RestAPI',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
